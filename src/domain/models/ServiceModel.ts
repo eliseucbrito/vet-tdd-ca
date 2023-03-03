d219d6683d28311f35d935861b16be5ffc01ba54
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-03 15:32:28 -0300

    feat: created ServiceModel

---
+++ src/domain/models/ServiceModel.ts
@@ -0,0 +1,46 @@
+import { PatientModel } from "./PatientModel"
+import { StaffModel } from "./StaffModel"
+
+export enum ServiceType {
+  EXAM,
+  MEDICAL_CARE,
+  HOME_CARE,
+  SURGERY,
+  EMERGENCY
+}
+
+export enum ServiceStatus {
+  SCHEDULED,
+  NOT_INITIALIZED,
+  IN_PROGRESS,
+  COMPLETED,
+  CANCELED
+}
+
+export enum PaymentStatus {
+  WAITING_PAYMENT,
+  PAID,
+  CANCELED
+}
+
+export enum City {
+  TRINDADE_PE,
+  ARARIPINA_PE,
+  OURICURI_PE
+}
+
+
+export type ServiceModel = {
+  id: number
+  title: string
+  description: string
+  createdAt: Date
+  serviceDate: Date
+  type: ServiceType
+  status: ServiceStatus
+  paymentStatus: PaymentStatus
+  city: City
+  price: number
+  staff: StaffModel
+  patient: PatientModel
+}
