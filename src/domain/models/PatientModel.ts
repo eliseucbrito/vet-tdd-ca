a2631e33164f47daf8f13fdc7c07dde288ae0837
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-03 10:25:35 -0300

    feat: created PatientModel and Kinds enum

---
+++ src/domain/models/PatientModel.ts
@@ -0,0 +1,20 @@
+export enum Kinds {
+  DOG,
+  CAT,
+  REPTILE,
+  BIRD,
+  FISH,
+  CATTLE
+}
+
+export type PatientModel = {
+  id: number
+  owner: string
+  createdAt: Date
+  name: string
+  ownerContact: number
+  breed: string
+  birthDate: Date
+  avatarUrl: string
+  kind: Kinds
+}
