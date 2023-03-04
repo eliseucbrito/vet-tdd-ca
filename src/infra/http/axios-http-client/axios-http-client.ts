808130b5cde06036db5dfc6adbe5ccf0c829e698
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-03 22:57:06 -0300

    feat: ensure AxiosHttpClient return a correct body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
-import { HttpPostParams } from "data/protocols/http";
+import { HttpPostClient, HttpPostParams, HttpResponse } from "data/protocols/http";
 
-export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
