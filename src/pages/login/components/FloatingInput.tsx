ba5a35f712ab6f12436dcc07f7d7ff372bf9bb25
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-06 17:45:51 -0300

    feat: floating input receiving ref correctly and can receive a right or left element

--- src/pages/login/components/FloatingInput.tsx
+++ src/pages/login/components/FloatingInput.tsx
@@ -1,19 +1,77 @@
-import { Input, InputProps } from '@chakra-ui/react'
+/* eslint-disable react/display-name */
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputProps,
+  Text,
+} from '@chakra-ui/react'
+import { forwardRef, ForwardRefRenderFunction, ReactNode } from 'react'
 
-export function FloatingInput({ ...InputProps }: InputProps) {
+interface FloatingInputProps extends InputProps {
+  label?: string
+  children?: ReactNode
+  errorMessage?: string
+  error?: boolean
+  name: string
+}
+
+const FloatingInputBase: ForwardRefRenderFunction<
+  HTMLInputElement,
+  FloatingInputProps
+> = (
+  { name, label, error, children, errorMessage = 'Campo inválido!', ...rest },
+  ref,
+) => {
   return (
-    <Input
-      type="text"
-      variant="flushed"
-      px={2}
-      _focus={{
-        'box-shadow': '0px 5px 10px 0px rgba(0, 0, 0, 0.5)',
-        borderRadius: 8,
-        border: 'none',
-        transform: 'translateY(-2px)',
-      }}
-      bg={['none', 'white']}
-      {...InputProps}
-    />
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      {children ? (
+        <InputGroup>
+          <Input
+            name={name}
+            id={name}
+            isInvalid={error}
+            variant="flushed"
+            px={2}
+            _focus={{
+              'box-shadow': '0px 5px 10px 0px rgba(0, 0, 0, 0.5)',
+              borderRadius: 8,
+              border: 'none',
+              transform: 'translateY(-2px)',
+            }}
+            ref={ref}
+            {...rest}
+          />
+
+          {children}
+        </InputGroup>
+      ) : (
+        <Input
+          name={name}
+          id={name}
+          isInvalid={error}
+          variant="flushed"
+          px={2}
+          _focus={{
+            'box-shadow': '0px 5px 10px 0px rgba(0, 0, 0, 0.5)',
+            borderRadius: 8,
+            border: 'none',
+            transform: 'translateY(-2px)',
+          }}
+          ref={ref}
+          {...rest}
+        />
+      )}
+      {error && (
+        <Text fontSize="sm" color="red">
+          {errorMessage}
+        </Text>
+      )}
+    </FormControl>
   )
 }
+
+export const FloatingInput = forwardRef(FloatingInputBase)
