b462a3267e72e39a5eadbcd7904e336f833e0306
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-06 01:30:27 -0300

    test: ensure inputs of login can be access

--- src/pages/login/login.spec.tsx
+++ src/pages/login/login.spec.tsx
@@ -1,28 +1,54 @@
 import React from 'react'
-import { render, RenderResult, screen } from '@testing-library/react'
+import {
+  cleanup,
+  fireEvent,
+  render,
+  RenderResult,
+  screen,
+} from '@testing-library/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { defaultTheme } from '../../presentation/styles/theme/defaultTheme'
 import Login from './index'
 import '../../presentation/tests/mock-match-media'
+import { Validation } from '../../presentation/protocols/validation'
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate(input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
+}
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
 }
 
 const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
   const sut = render(
     <ChakraProvider theme={defaultTheme}>
-      <Login />
+      <Login validation={ValidationSpy} />
     </ChakraProvider>,
   )
 
   return {
     sut,
+    validationSpy,
   }
 }
 
 describe('Login component', () => {
-  test('', () => {
-    const sut = makeSut()
+  afterEach(cleanup)
+  test('Should call validation with correct value', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email-input')
+    fireEvent.input(emailInput, { target: { value: 'my-email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'my-email',
+    })
   })
 })
