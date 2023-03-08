84d345ce1b55182f3fc4b157bc8d2a74db3f2c13
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-08 16:52:08 -0300

    feat: sidebar button

---
+++ src/presentation/components/Sidebar/SidebarButton.tsx
@@ -0,0 +1,34 @@
+import { Flex, Icon, Show, Text } from '@chakra-ui/react'
+import { IconType } from 'react-icons'
+import Link from 'next/link'
+import { useContext } from 'react'
+import { SidebarContext } from './Sidebar'
+import { useRouter } from 'next/router'
+
+interface SidebarButtonProps {
+  label: string
+  icon: IconType
+  href: string
+}
+
+export function SidebarButton({ icon, label, href }: SidebarButtonProps) {
+  const { sidebarOpen } = useContext(SidebarContext)
+  const { asPath } = useRouter()
+  const activeLink = asPath === href
+
+  return (
+    <Flex
+      bg={activeLink ? 'green.300' : 'transparent'}
+      as={Link}
+      href={href}
+      w="100%"
+      p={2}
+      gap={2}
+      align="center"
+      borderRadius={8}
+    >
+      <Icon as={icon} boxSize={6} />
+      <Text display={sidebarOpen ? 'block' : 'none'}>{label}</Text>
+    </Flex>
+  )
+}
