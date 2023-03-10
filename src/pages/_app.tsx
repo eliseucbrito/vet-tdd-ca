8edb63b3e8ab4ed185d9f24a7b808147f46dc59b
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-09 22:29:29 -0300

    feat: add QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { ChakraProvider, Flex } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { QueryClientProvider } from 'react-query'
 import { defaultTheme } from 'presentation/styles/theme/defaultTheme'
+import { queryClient } from 'presentation/services/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={defaultTheme}>
-      <Flex w="100vw" h="100vh" overflow="auto">
-        <Component {...pageProps} />
-      </Flex>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={defaultTheme}>
+        <Flex w="100vw" h="100vh" overflow="auto">
+          <Component {...pageProps} />
+        </Flex>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
