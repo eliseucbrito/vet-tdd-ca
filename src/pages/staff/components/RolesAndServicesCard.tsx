a5565d5428b07aea1090bdc375d4699bac828977
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-12 11:27:23 -0300

    refactor: rename component StaffServices to ServicesDetailsCard

--- src/pages/staff/components/RolesAndServicesCard.tsx
+++ src/pages/staff/components/RolesAndServicesCard.tsx
@@ -2,13 +2,13 @@ import { Flex, HStack, Text, VStack } from '@chakra-ui/react'
 import { CheckBar } from 'presentation/components/Cards/CheckBar'
 import { useState } from 'react'
 import { RoleHistoricCard } from './RoleHistoricCard'
-import { StaffServicesCard } from './StaffServicesCard'
+import { ServicesDetailsCard } from './ServicesDetailsCard'
 
 export function RolesAndServicesCard() {
   const [displayedData, setDisplayedData] = useState(1)
 
   return (
-    <VStack align="start" w="100%" bg="white" p={4} borderRadius={12}>
+    <VStack align="start" minW="100%" bg="white" p={4} borderRadius={12}>
       <HStack bg="gray.300" p={2} mb={2} borderRadius={6}>
         <Text
           as={'button'}
@@ -40,7 +40,7 @@ export function RolesAndServicesCard() {
         ) : (
           <HStack w="100%" gap={10} justify="space-between">
             <CheckBar completed />
-            <StaffServicesCard />
+            <ServicesDetailsCard />
           </HStack>
         )}
       </VStack>
