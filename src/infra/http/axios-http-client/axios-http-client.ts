00321e96266b2629a250253f9e5ac95b98e44d0d
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-14 17:10:17 -0300

    feat: failedRequests queue

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -6,6 +6,12 @@ import {
 } from './../../../data/protocols/http/http-client'
 import { parseCookies, setCookie } from 'nookies'
 
+let isRefreshing = false
+let failedRequestsQueue: {
+  onSuccess: (token: string) => void
+  onFailure: (err: AxiosError<unknown, any>) => void
+}[] = []
+
 const cookies = parseCookies()
 export const api = axios.create({
   baseURL: 'http://localhost:80',
@@ -21,33 +27,71 @@ api.interceptors.response.use(
   (error: AxiosError<{ message: string }>) => {
     if (error.response.status === 401) {
       if (error.response.data?.message === 'token.expired') {
-        console.log('FAZENDO REFRESH')
+        const originalConfig = error.config!
 
-        api
-          .put('/auth/refresh', null, {
-            headers: {
-              Authorization: `Bearer ${cookies['vet.refreshToken']}`,
+        if (!isRefreshing) {
+          isRefreshing = true
+
+          api
+            .put('/auth/refresh', null, {
+              headers: {
+                Authorization: `Bearer ${cookies['vet.refreshToken']}`,
+              },
+            })
+            .then((response) => {
+              const { accessToken, refreshToken } = response.data
+
+              setCookie(undefined, 'vet.token', accessToken, {
+                maxAge: 60 * 60 * 24, // 24h
+                path: '/',
+              })
+
+              setCookie(undefined, 'vet.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24, // 24h
+                path: '/',
+              })
+              api.defaults.headers.Authorization = `Bearer ${accessToken}`
+
+              failedRequestsQueue.forEach((request) =>
+                request.onSuccess(accessToken),
+              )
+              failedRequestsQueue = []
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request) => request.onFailure(err))
+              failedRequestsQueue = []
+
+              if (typeof window !== 'undefined') {
+                // signOut()
+              }
+            })
+            .finally(() => {
+              isRefreshing = false
+            })
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              ;(originalConfig.headers as AxiosHeaders).set(
+                'Authorization',
+                `Bearer ${token}`,
+              )
+
+              resolve(api(originalConfig!))
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err)
             },
           })
-          .then((response) => {
-            const { accessToken, refreshToken } = response.data
-
-            setCookie(undefined, 'vet.token', accessToken, {
-              maxAge: 60 * 60 * 24, // 24h
-              path: '/',
-            })
-
-            setCookie(undefined, 'vet.refreshToken', refreshToken, {
-              maxAge: 60 * 60 * 24, // 24h
-              path: '/',
-            })
-
-            console.log('REFRESH FEITO')
-            api.defaults.headers.Authorization = `Bearer ${accessToken}`
-          })
+        })
       } else {
         // deslogar
       }
+
+      return Promise.reject(error)
+    } else {
+      throw error
     }
   },
 )
