6c851a62f810f691eecd93957b2d5cca1a7abc1f
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-14 23:56:58 -0300

    feat: staff page and staff details completed UI

--- src/pages/staff/[id].tsx
+++ src/pages/staff/[id].tsx
@@ -1,17 +1,43 @@
-import { HStack, VStack } from '@chakra-ui/react'
-import { DetailsCard } from 'presentation/components/Cards/DetailsCard'
+import { HStack, Spinner, VStack } from '@chakra-ui/react'
+import { AxiosHttpClient } from 'infra/http/axios-http-client/axios-http-client'
+import { GetServerSideProps } from 'next'
+import { StaffDetailsCard } from 'presentation/components/Cards/StaffDetailsCard'
 import { RolesAndServicesCard } from './components/RolesAndServicesCard'
 import { StaffCard } from './components/StaffCard'
+import { StaffModel } from 'domain/models/StaffModel'
 
-export default function StaffDetails() {
+interface StaffDetailsProps {
+  staff: StaffModel
+}
+
+export default function StaffDetails({ staff }: StaffDetailsProps) {
   return (
     <VStack p="1rem 1rem 1rem 1.5rem" gap={4} w="100%">
       <HStack w="100%">
-        <StaffCard />
-        <DetailsCard />
+        <StaffCard staff={staff} />
+        <StaffDetailsCard staff={staff} />
       </HStack>
 
       <RolesAndServicesCard />
     </VStack>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { id } = ctx.params
+  const axios = new AxiosHttpClient()
+  const { body: staff } = await axios.request({
+    method: 'get',
+    url: `/api/staff/v1/${id}`,
+    headers: {
+      Authorization:
+        'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsZWFuZHJvIiwicm9sZXMiOlsiQ0VPIiwiR0VORVJBTF9NQU5BR0VSIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJleHAiOjE2Nzg2Nzk4ODMsImlhdCI6MTY3ODY3NjI4M30.oic7PLQQ332DbpIN36nrBHL5E8Iqvt3MuiJk4WveHzU',
+    },
+  })
+
+  return {
+    props: {
+      staff,
+    },
+  }
 }
--- src/pages/staff/index.tsx
+++ src/pages/staff/index.tsx
@@ -1,8 +1,19 @@
-import { Wrap, WrapItem } from '@chakra-ui/react'
+import { Spinner, Wrap, WrapItem } from '@chakra-ui/react'
+import { ServiceModel } from 'domain/models/ServiceModel'
 import React from 'react'
 import { StaffCard } from './components/StaffCard'
+import Link from 'next/link'
+import { GetServerSideProps } from 'next/types'
+import { AxiosHttpClient } from './../../infra/http/axios-http-client/axios-http-client'
+import { StaffModel } from 'domain/models/StaffModel'
 
-export default function Staff() {
+interface StaffProps {
+  staffs: StaffModel[]
+}
+
+export default function Staff({ staffs }: StaffProps) {
+  console.log(staffs)
+
   return (
     <Wrap
       flexWrap="wrap"
@@ -12,33 +23,36 @@ export default function Staff() {
       h="100%"
       spacing={8}
     >
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
-      <WrapItem>
-        <StaffCard rounded />
-      </WrapItem>
+      {staffs === undefined ? (
+        <Spinner />
+      ) : (
+        staffs.map((staff) => (
+          <WrapItem key={staff.id}>
+            <Link href={`/staff/${staff.id}`}>
+              <StaffCard rounded staff={staff} />
+            </Link>
+          </WrapItem>
+        ))
+      )}
     </Wrap>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const axios = new AxiosHttpClient()
+  const { body: staffs } = await axios.request({
+    method: 'get',
+    url: '/api/staff/v1',
+    headers: {
+      Authorization:
+        'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsZWFuZHJvIiwicm9sZXMiOlsiQ0VPIiwiR0VORVJBTF9NQU5BR0VSIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJleHAiOjE2Nzg2NzkyNzIsImlhdCI6MTY3ODY3NTY3Mn0.qUfMHyXZOGAr_jc1iBZvEg_J3PdAaWlpnNNQUcJbf5Y',
+    },
+  })
+  console.log(staffs)
+
+  return {
+    props: {
+      staffs,
+    },
+  }
 }
