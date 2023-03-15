47e3a0c21787fe2877a20c2492d89da4ddc35c7d
Eliseu Brito <eliseubrito776@gmail.com>
2023-03-14 23:56:26 -0300

    feat: call api in SSR for get patient details

--- src/pages/patients/[id].tsx
+++ src/pages/patients/[id].tsx
@@ -4,6 +4,7 @@ import { ServicesCard } from './components/ServicesCard'
 import { GetServerSideProps } from 'next/types'
 import { AxiosHttpClient } from './../../infra/http/axios-http-client/axios-http-client'
 import { PatientDetailsCard } from 'presentation/components/Cards/PatientDetailsCard'
+import { parseCookies } from 'nookies'
 
 export default function StaffDetails({ patient }) {
   return (
@@ -21,12 +22,12 @@ export default function StaffDetails({ patient }) {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { id } = ctx.params
   const axios = new AxiosHttpClient()
+  const { 'vet.token': token } = parseCookies(ctx)
   const { body: patient } = await axios.request({
     method: 'get',
     url: `/api/patients/v1/${id}`,
     headers: {
-      Authorization:
-        'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsZWFuZHJvIiwicm9sZXMiOlsiQ0VPIiwiR0VORVJBTF9NQU5BR0VSIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJleHAiOjE2Nzg2NzQzNTMsImlhdCI6MTY3ODY3MDc1M30.tX-LcneDHoFdYpL6OeaQ08y1ddKFlCsYqJ3xG7t5Zm0',
+      Authorization: `Bearer ${token}`,
     },
   })
 
