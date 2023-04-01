f2ac4e2d999923d4964c04d7f6af6278e11f5a4b
Eliseu Brito <eliseubrito776@gmail.com>
2023-04-01 13:39:50 -0300

    feat: change attribute username to email, for send the credentials in login (commit out of order)

--- src/presentation/protocols/SignIn.ts
+++ src/presentation/protocols/SignIn.ts
@@ -3,8 +3,9 @@ import {
   AxiosHttpClient,
 } from './../../infra/http/axios-http-client/axios-http-client'
 import { setCookie } from 'nookies'
-import { StaffReduced } from 'domain/models/StaffModel'
+import { StaffModel, StaffReduced } from 'domain/models/StaffModel'
 import { HttpResponse } from 'data/protocols/http'
+import { AxiosResponse } from 'axios'
 
 type credentialsProps = {
   email: string
@@ -12,18 +13,20 @@ type credentialsProps = {
   rememberMe: boolean
 }
 
-export async function SignIn(credentials: credentialsProps) {
+export async function SignIn(
+  credentials: credentialsProps,
+): Promise<AxiosResponse> {
   const axios = new AxiosHttpClient()
   const url = '/auth/signin'
   const method = 'post'
   const body = {
-    username: credentials.email,
+    email: credentials.email,
     password: credentials.password,
   }
 
   const maxAgeValue = credentials.rememberMe ? 60 * 60 * 24 * 30 : 60 * 60 * 24
 
-  axios
+  await axios
     .authentication({
       url,
       method,
@@ -33,19 +36,31 @@ export async function SignIn(credentials: credentialsProps) {
       },
     })
     .then((response) => {
-      api.defaults.headers.Authorization = `Bearer ${response.body.accessToken}`
+      api.defaults.headers.common.Authorization = `Bearer ${response.body.accessToken}`
+
+      console.log('ACCESSS TOKEN ', response.body.accessToken)
 
       setCookie(undefined, 'vet.token', response.body.accessToken, {
         maxAge: maxAgeValue,
         path: '/',
+        httpOnly: false,
       })
 
       setCookie(undefined, 'vet.refreshToken', response.body.refreshToken, {
         maxAge: maxAgeValue,
         path: '/',
+        httpOnly: false,
       })
     })
     .catch((error) => {
       throw error
     })
+
+  const user = await api.get<StaffModel>('/api/staff/v2/me', {
+    headers: {
+      Authorization: api.defaults.headers.common.Authorization,
+    },
+  })
+
+  return user
 }
