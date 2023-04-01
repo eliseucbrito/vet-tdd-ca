863e6c54fa10dfc113c39330ebb69472becb00e2
Eliseu Brito <eliseubrito776@gmail.com>
2023-04-01 13:36:38 -0300

    refactor: add formatters and receiving the services to show

--- src/pages/staff/components/ServicesDetailsCard.tsx
+++ src/pages/staff/components/ServicesDetailsCard.tsx
@@ -8,12 +8,22 @@ import {
   Td,
   Text,
 } from '@chakra-ui/react'
+import dayjs from 'dayjs'
+import { ServiceModel } from 'domain/models/ServiceModel'
 import Link from 'next/link'
+import { cityFormatter } from 'presentation/utils/cityFormatter'
+import { nameFormatter } from 'presentation/utils/nameFormatter'
+import { serviceStatusFormatter } from 'presentation/utils/serviceStatusFormatter'
+import { serviceTypeFormatter } from 'presentation/utils/serviceTypeFormatter'
 import { ReactNode } from 'react'
 
 interface ServiceDetailsBlockProps {
   children: ReactNode
   last?: boolean
+}
+
+interface ServicesDetailsCardProps {
+  services: ServiceModel[]
 }
 
 function ServiceDetailsBlock({ children, last }: ServiceDetailsBlockProps) {
@@ -26,7 +36,7 @@ function ServiceDetailsBlock({ children, last }: ServiceDetailsBlockProps) {
   )
 }
 
-export function ServicesDetailsCard() {
+export function ServicesDetailsCard({ services }: ServicesDetailsCardProps) {
   return (
     <Table
       sx={{
@@ -36,57 +46,74 @@ export function ServicesDetailsCard() {
       scrollBehavior={'auto'}
     >
       <Tbody>
-        <Tr
-          sx={{
-            td: {
-              background: 'white',
-              whiteSpace: 'nowrap',
-              '&:first-of-type': {
-                borderLeftRadius: '12px',
-              },
-              '&:last-of-type': {
-                borderRightRadius: '12px',
-              },
-            },
-          }}
-        >
-          <Td px={2} textAlign="center">
-            <ServiceDetailsBlock>
-              <Text>23 Jan'23</Text>
-              <Text variant="subtitle-12" lineHeight={0.5}>
-                Concluído
-              </Text>
-            </ServiceDetailsBlock>
-          </Td>
+        {services === undefined ? (
+          <Text>Ainda sem serviços registrados</Text>
+        ) : (
+          services.map((service) => {
+            return (
+              <Tr
+                key={service.id}
+                sx={{
+                  td: {
+                    background: 'white',
+                    whiteSpace: 'nowrap',
+                    '&:first-of-type': {
+                      borderLeftRadius: '12px',
+                    },
+                    '&:last-of-type': {
+                      borderRightRadius: '12px',
+                    },
+                  },
+                }}
+              >
+                <Td px={2} textAlign="center">
+                  <ServiceDetailsBlock>
+                    <Text>
+                      {dayjs(service.serviceDate).format("DD MMMM[']YY")}
+                    </Text>
+                    <Text variant="subtitle-12" lineHeight={0.5}>
+                      {serviceStatusFormatter(service.status)}
+                    </Text>
+                  </ServiceDetailsBlock>
+                </Td>
 
-          <Td px={2} textAlign="center">
-            <ServiceDetailsBlock>
-              <Text>ID</Text>
-              <Text fontWeight={600}>19</Text>
-            </ServiceDetailsBlock>
-          </Td>
+                <Td px={2} textAlign="center">
+                  <ServiceDetailsBlock>
+                    <Text>ID</Text>
+                    <Text fontWeight={600}>{service.id}</Text>
+                  </ServiceDetailsBlock>
+                </Td>
 
-          <Td px={2} textAlign="center">
-            <ServiceDetailsBlock>
-              <Text>Cidade</Text>
-              <Text fontWeight={600}>Jaqueriguaguara</Text>
-            </ServiceDetailsBlock>
-          </Td>
+                <Td px={2} textAlign="center">
+                  <ServiceDetailsBlock>
+                    <Text>Cidade</Text>
+                    <Text fontWeight={600}>
+                      {cityFormatter(service.city.name)}
+                    </Text>
+                  </ServiceDetailsBlock>
+                </Td>
 
-          <Td px={2} textAlign="center">
-            <ServiceDetailsBlock>
-              <Text>Serviço</Text>
-              <Text fontWeight={600}>Atendimento Médico</Text>
-            </ServiceDetailsBlock>
-          </Td>
+                <Td px={2} textAlign="center">
+                  <ServiceDetailsBlock>
+                    <Text>Serviço</Text>
+                    <Text fontWeight={600}>
+                      {serviceTypeFormatter(service.type)}
+                    </Text>
+                  </ServiceDetailsBlock>
+                </Td>
 
-          <Td px={2} textAlign="center">
-            <ServiceDetailsBlock last>
-              <Text>Paciente</Text>
-              <Text fontWeight={600}>Jubilau Santo</Text>
-            </ServiceDetailsBlock>
-          </Td>
-        </Tr>
+                <Td px={2} textAlign="center">
+                  <ServiceDetailsBlock last>
+                    <Text>Médico</Text>
+                    <Text fontWeight={600}>
+                      {nameFormatter(service.medic.fullName)}
+                    </Text>
+                  </ServiceDetailsBlock>
+                </Td>
+              </Tr>
+            )
+          })
+        )}
       </Tbody>
     </Table>
   )
