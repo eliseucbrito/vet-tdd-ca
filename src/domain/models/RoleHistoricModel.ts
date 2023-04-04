240a24f6709837c03731744a567c0c775305d031
Eliseu Brito <eliseubrito776@gmail.com>
2023-04-03 21:54:39 -0300

    refactor: change StaffModel to StaffReduced on promoter of role and added values to enum

--- src/domain/models/RoleHistoricModel.ts
+++ src/domain/models/RoleHistoricModel.ts
@@ -1,18 +1,22 @@
-import { StaffModel } from './StaffModel'
+import { StaffModel, StaffReduced } from './StaffModel'
 
 export enum Roles {
-  CEO,
-  GENERAL_MANAGER,
-  MANAGER,
-  VETERINARY,
-  ASSISTANT,
-  INTERN,
+  CEO = 'CEO',
+  GENERAL_MANAGER = 'GENERAL_MANAGER',
+  MANAGER = 'MANAGER',
+  VETERINARY = 'VETERINARY',
+  ASSISTANT = 'ASSISTANT',
+  INTERN = 'INTERN',
 }
 
 export type RoleModel = {
-  startedIn: Date
-  role: Roles
+  id: number
+  startedIn: string
+  role: {
+    id: number
+    description: Roles
+  }
   baseSalary: number
   weeklyWorkLoad: number
-  promoter: StaffModel
+  promoter: StaffReduced
 }
