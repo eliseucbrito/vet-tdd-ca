021ebfc6aa84d46bbf652e30a329a615d4af26f1
eliseubrito7z <eliseubrito776@gmail.com>
2023-04-29 23:33:00 -0300

    refactor: add animation to graphic

--- src/pages/finances/components/charts/BarChart.tsx
+++ src/pages/finances/components/charts/BarChart.tsx
@@ -18,6 +18,19 @@ export function BarChart() {
       toolbar: {
         show: false,
       },
+      animations: {
+        enabled: true,
+        easing: 'easeinout',
+        speed: 800,
+        animateGradually: {
+          enabled: true,
+          delay: 150,
+        },
+        dynamicAnimation: {
+          enabled: true,
+          speed: 350,
+        },
+      },
     },
     tooltip: {
       style: {
